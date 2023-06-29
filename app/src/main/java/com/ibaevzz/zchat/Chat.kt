0888dbbe3d3f30f103ff2af468cb95e6005fdeff
Ibaev Zubai <ibaevzubic2004@gmail.com>
2023-06-30 00:53:06 +0300

    Что-то сделано

---
+++ app/src/main/java/com/ibaevzz/zchat/Chat.kt
@@ -0,0 +1,5 @@
+package com.ibaevzz.zchat
+
+import android.graphics.Bitmap
+
+data class Chat(val name: String, val number: String, val image: Bitmap)
--- app/src/main/java/com/ibaevzz/zchat/MainActivity.kt
+++ app/src/main/java/com/ibaevzz/zchat/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.ibaevzz.zchat
 
-import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ibaevzz.zchat.databinding.ActivityMainBinding
+import com.ibaevzz.zchat.databinding.ChatLayoutBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +20,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        val chats = getChats()
+
+        if(chats.isEmpty()){
+            binding.isChat.visibility = View.VISIBLE
+            binding.chats.visibility = View.INVISIBLE
+        }else{
+            binding.chats.layoutManager = LinearLayoutManager(this)
+            binding.chats.adapter = Adapter(chats)
+        }
+    }
+
+    private fun getChats(): List<Chat>{
+        //TODO
+        return emptyList()
+    }
+
+    private class Holder(private val chatLayoutBinding: ChatLayoutBinding): RecyclerView.ViewHolder(chatLayoutBinding.root){
+        fun makeChat(chat:Chat){
+            //TODO
+        }
+    }
+
+    private class Adapter(private val chats: List<Chat>): RecyclerView.Adapter<Holder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val chatBinding = ChatLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return Holder(chatBinding)
+        }
+
+        override fun getItemCount(): Int {
+            return chats.size
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.makeChat(chats[position])
+        }
+
     }
 }
